dd4b73dc952a856b23050615ee349f904a05352a
Rafal Wysocki <wys.rafal@gmail.com>
2018-05-18 02:16:09 +0200

    logo scales properly

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const header = document.querySelector('.page-header');
     window.addEventListener('scroll', function(e) {
-        if (this.pageYOffset >= 100) {
+        if (this.pageYOffset >= 20) {
             header.classList.add('page-header-shrink');
         } else {
             header.classList.remove('page-header-shrink');
