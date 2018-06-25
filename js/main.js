4e7ae78e9ac223eb9e8dd3b1006c11be5864aa15
Rafal Wysocki <wys.rafal@gmail.com>
2018-06-25 23:57:18 +0200

    working on lightbox

--- js/main.js
+++ js/main.js
@@ -16,48 +16,108 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (var i = 0; i < list.length; i++) {
         function showFullscreen(i) {
-            list[i].addEventListener("click", function () {
-
-                var src = (this.children[0].src);
+            function createFullscreen(i) {
                 var element = document.createElement("div");
 
                 element.classList.add("fullScreen");
 
-                element.innerHTML =
-                    '<button class="gallery-btn gallery-prev"></button>' +
-                    '<button class="gallery-btn gallery-next"></button>' +
-                    '<button class="close"></button>' +
-                    '<img src="' + src + '">';
+                function addContent(listElement) {
+                    var src = (listElement.children[0].src);
+                    element.innerHTML =
+                        '<button class="gallery-btn gallery-prev"></button>' +
+                        '<button class="gallery-btn gallery-next"></button>' +
+                        '<button class="close"></button>' +
+                        '<img class="gallery-img" src="' + src + '">';
+                }
 
                 body.appendChild(element);
-
-                var close = element.querySelector(".close");
+                addContent(list[i]);
+                closeFullscreen(element)
+            }
+            function closeFullscreen(element) {
+                var close = document.querySelector(".close");
                 close.addEventListener("click", function () {
                     body.removeChild(element);
 
                 });
+
+            }
+
+
+            list[i].addEventListener("click", function () {
+                createFullscreen(i);
+
+
+
+                var previous = document.querySelector(".gallery-prev");
+                var j = 0;
+                previous.addEventListener("click", function () {
+                    j--;
+                    // addContent(list[i - j]);
+                    createFullscreen(i--);
+                    console.log(j);
+                });
+
+
+                // var close = document.querySelector(".close");
+                // close.addEventListener("click", function () {
+                //     body.removeChild(element);
+                //
+                // });
             });
 
         }
         showFullscreen(i);
 
-        // var previous = document.querySelector(".gallery-prev");
-        // previous.addEventListener("click", function () {
-        //     console.log("xd");
-        //     // document.querySelector(".fullscreen").removeChild("img");
-        //     // element.appendChild("img")
-        //     showFullscreen(i++)
-        //
-        //
-        // })
     }
+    //
+    // for (var i = 0; i < list.length; i++) {
+    //         function showFullscreen(i) {
+    //             list[i].addEventListener("click", function () {
+    //
+    //                 var src = (this.children[0].src);
+    //                 var element = document.createElement("div");
+    //
+    //                 element.classList.add("fullScreen");
+    //
+    //                 element.innerHTML =
+    //                     '<button class="gallery-btn gallery-prev"></button>' +
+    //                     '<button class="gallery-btn gallery-next"></button>' +
+    //                     '<button class="close"></button>' +
+    //                     '<img class="gallery-img" src="' + src + '">';
+    //
+    //                 body.appendChild(element);
+    //
+    //                 var close = document.querySelector(".close");
+    //                 close.addEventListener("click", function () {
+    //                     body.removeChild(element);
+    //
+    //                 });
+    //
+    //             });
+    //
+    //         }
+    //         showFullscreen(i);
+    //
+    //     }
     // var previous = document.querySelector(".gallery-prev");
-    // var fullscreen = document.querySelector(".fullscreen");
+    // var fullScreen = document.querySelector(".fullScreen");
+    // var img = document.querySelector(".gallery-img");
+    // var j = 0;
     // previous.addEventListener("click", function () {
-    //     fullscreen.removeChild("img")
+    //     j--;
+    //     console.log(j);
+    //     fullScreen.removeChild(img);
+    //     fullScreen.innerHTML = '<button class="gallery-btn gallery-prev"></button>' +
+    //         '<button class="gallery-btn gallery-next"></button>' +
+    //         '<button class="close"></button>' +
+    //         '<img class="gallery-img" src="' + list[i+j].children[0].src + '">';
     //
     //
-    // })
+    // });
+
+
+
 
 });
 
